f0ec8874e7a03101ef1caa73a80bc1796c1f9a76
Vishwesh <vishwesh.h@gmail.com>
2016-12-29 04:04:18 +0530

    Changed the Down Stream App to host dummy service
    Dummy Authorization Service added to mock external dependency on Passport

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Random;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -12,10 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.model.User;
 
 @SpringBootApplication
 @RestController
@@ -36,14 +36,14 @@ public class DemoApplication {
 		return list;
 	}
     
-	@RequestMapping(value = "/User")
-    public @ResponseBody User getAllEmployees() 
+	@RequestMapping(value = "/api/Authorizaton/IsAuthorized")
+    public @ResponseBody Boolean getAuthorization(@RequestParam("lanId") String lanId, @RequestParam("applicationName") String applicationName) 
     {
         log.info("Time - " + Calendar.getInstance().getTime());
-
-        User user = new User(1,"Vishwesh","H","vishwesh.h@cqent.biz", "AppealAssistant", true, "BIL7446");
-        
-        return user;
+        Random random = new Random();
+        Boolean result = random.nextBoolean();
+        System.out.println("result @ " + result);
+        return result;
     }
 	
 	public static void main(String[] args) {
